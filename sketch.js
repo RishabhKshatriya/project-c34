fa0d6c58b4a320425c65be19573220104c31a162
RishabhKshatriya <68421561+RishabhKshatriya@users.noreply.github.com>
2020-09-23 21:11:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog,dogdog_img,foodstock,happys_img;
+var database
+
+function preload()
+{
+  dog_img = loadImage('sprites/dog1mg.png');
+	//load images here
+}
+
+function setup() {
+  createCanvas(500,500);
+dog = createSprite(200,200,50,30);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
